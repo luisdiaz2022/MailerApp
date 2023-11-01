f32231f250fd3c4b2d40808cfd9633faa909ffb5
Luis <luisedgucv@gmail.com>
2023-11-01 14:25:07 -0500

    Creaed file for the database and connect it with the main app

---
+++ app/db.py
@@ -0,0 +1,43 @@
+import mysql.connector
+
+import click
+from flask import current_app, g
+from flask.cli import with_appcontext
+from .schema import instructions
+
+def get_db():
+    if 'db' not in g:
+        g.db = mysql.connector.connect(
+            host=current_app.config['DATABASE_HOST'],
+            user=current_app.config['DATABASE_USER'],
+            password=current_app.config['DATABASE_PASSWORD'],
+            database=current_app.config['DATABASE']
+        )
+        g.cursor = g.db.cursor(dictionary=True)
+    return g.db, g.cursor
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+def init_db():
+    db, cursor = get_db()
+
+    for i in instructions:
+        cursor.execute(i)
+
+    db.commit()
+
+
+@click.command('init-db')
+@with_appcontext
+
+def init_db_commmand():
+    init_db()
+    click.echo("Base de datos inicializada")
+
+def init_app(app):
+    app.teardow_appcontext(close_db)
+    app.cli.add_command(init_db_commmand)
