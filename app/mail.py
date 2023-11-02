17c3dcc854de023ee3bab60b6ef3b5cca888e850
Luis <luisedgucv@gmail.com>
2023-11-02 11:50:10 -0500

    Added flash errors in case entry points are empty and added styles to it

--- app/mail.py
+++ app/mail.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, render_template
+    Blueprint, render_template, request, flash
 )
 
 from app.db import get_db
@@ -17,4 +17,23 @@ def index():
 
 @bp.route('/create', methods=['GET', 'POST'])
 def create():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        subject = request.form.get('subject')
+        content = request.form.get('content')
+        errors = []
+
+        if not email:
+            errors.append('Correo es obligatorio')
+        if not subject:
+            errors.append('Asunto es obligatorio')
+        if not content:
+            errors.append('Contenido es obligatorio')
+
+        if len(errors) == 0:
+            pass
+        else:
+            for error in errors:
+                flash(error)
+
     return render_template('mails/create.html')
